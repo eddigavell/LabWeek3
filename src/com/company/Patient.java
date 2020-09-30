8a8073f4be04822d008b124873bc81da287ccb23
Edvin Gavell <eddi@live.se>
2020-09-30 08:01:32 +0200

    Cleaned up code and made some improvements.

--- src/com/company/Patient.java
+++ src/com/company/Patient.java
@@ -3,46 +3,7 @@ package com.company;
 class Patient {
     private String name;
     private String sickness;
-    private boolean isSick;
 
-    Patient(String name, String sickness) {
-        this.name = name;
-        if (sickness == null || sickness.equals("")) {
-            this.isSick = false;
-            this.sickness = "";
-        } else {
-            this.sickness = sickness;
-            this.isSick = true;
-        }
-    }
-
-    Patient(String name) {
-        this(name,"");
-    }
-
-    boolean isSick() {
-        return this.isSick;
-    }
-
-    String getSickness() {
-        return this.sickness;
-    }
-
-    String getName() {
-        return this.name;
-    }
-
-    void takeMedication(Medicine name) {
-       if (this.sickness.equals(name.getTreatmentName())) {
-           this.isSick = false;
-       }
-    }
-
-}
-/*
-class Patient {
-    private String name;
-    private String sickness;
 
     Patient(String name, String sickness) {
         this.name = name;
@@ -50,11 +11,7 @@ class Patient {
     }
 
     Patient(String name) {
-        this(name, ""); //"" ersätta med null -> NullPointerException...
-    }
-
-    boolean isSick() {
-        return this.sickness != null && !this.sickness.equals("");
+        this(name,null);
     }
 
     String getSickness() {
@@ -65,11 +22,13 @@ class Patient {
         return this.name;
     }
 
+    boolean isSick() {
+        return this.sickness != null;
+    }
+
     void takeMedication(Medicine name) {
-        if (this.sickness.equals(name.getTreatmentName())) {
+        if (isSick() && this.sickness.equals(name.getTreatmentName())) {
             this.sickness = null;
         }
     }
-
 }
-*/
